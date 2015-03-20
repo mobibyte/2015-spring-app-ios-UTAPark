a90ea2b5f1a4f2ed559e3c21051c1dbee531c713
Jstaud <james.staud@mavs.uta.edu>
2015-03-20 14:04:34 -0500

    initial commit

---
+++ UTA-Park/UTA-Park/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UTA-Park
+//
+//  Created by James Staud on 3/20/15.
+//  Copyright (c) 2015 Mobi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UTA-Park/UTA-Park/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UTA-Park
+//
+//  Created by James Staud on 3/20/15.
+//  Copyright (c) 2015 Mobi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
